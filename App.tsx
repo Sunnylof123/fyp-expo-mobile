39b31d6a320fd6fa7498aa93fac2e332e9ba10cf
LWL220080946 <sunnywong.family@gmail.com>
2024-03-04 22:38:27 +0800

    init project and add file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View } from "react-native"
+import WebView from "react-native-webview"
+const url = "http://192.168.1.50:3000"
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView
+        style={{ width: "100%", height: "100%" }}
+        source={{ uri: "http://192.168.1.50:3000" }}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+})
