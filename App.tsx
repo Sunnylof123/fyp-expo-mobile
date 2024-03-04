3a4d401378e871aac6c1a6510c5907d8faacd85f
LWL220080946 <sunnywong.family@gmail.com>
2024-03-04 22:41:23 +0800

    update app.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StyleSheet, Text, View } from "react-native"
 import WebView from "react-native-webview"
-const url = "http://192.168.1.50:3000"
+const url = "http://" + "your react app doamin "
 export default function App() {
   return (
     <View style={styles.container}>
       <WebView
         style={{ width: "100%", height: "100%" }}
-        source={{ uri: "http://192.168.1.50:3000" }}
+        source={{ uri: url }}
       />
     </View>
   )
