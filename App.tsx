e0e7a123ff1d11664aaf322b11e59a69098f4259
LWL220080946 <sunnywong.family@gmail.com>
2024-04-11 11:02:00 +0800

    update

--- App.tsx
+++ App.tsx
@@ -18,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 45,
-    paddingBottom: 30,
+    paddingTop: 55,
+    paddingBottom: 20,
     backgroundColor: "black"
   },
 })
